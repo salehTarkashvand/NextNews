c33d13f6593ec245a514c8d4d0f6a0f31998fecb
ewdwins <saleh.tarkashvand@gmail.com>
2025-09-03 15:52:48 +0330

    App Styling & Using Dummy Data

---
+++ app/news/[id]/page.js
@@ -0,0 +1,9 @@
+export default function NewsDetailsPage ({params}){
+    const newId = params.id
+    return (
+      <>
+      <p> new id = {newId} </p>
+        <p>MealDetailsPage</p>
+      </>
+    );
+}
