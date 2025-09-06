eca4eff011dfadf7d8e1c9d2fa44ff171b417e7d
ewdwins <saleh.tarkashvand@gmail.com>
2025-09-06 13:19:41 +0330

    Configuring Catch-All Routes & Catch-All Fallback Routes & Dealing With Multiple Path Segments

--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getAvailableNewsMonths, getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-  const newsYear = params.year;
-  const year = getNewsForYear(newsYear);
-  return <NewsList news={year} />;
-}
